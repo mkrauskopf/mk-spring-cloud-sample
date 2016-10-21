f89c4699d10065b1c98ca33bd7b6ec67c4be6a9a
Martin Krauskopf <martin.krauskopf@gmail.com>
2016-10-21 13:52:22 +0200

    Minor code clarification: better naming.

--- gateway/src/main/java/cz/martyn/cloud/number/filter/WhiteListFilter.java
+++ gateway/src/main/java/cz/martyn/cloud/number/filter/WhiteListFilter.java
@@ -11,13 +11,13 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
 /**
- * White list filter.
- * <p>
- * Created by mkalinovits on 10/18/16.
+ * Filter which let pass calls to application registry service only by services on whitelist.
  */
 public class WhiteListFilter extends ZuulFilter {
 
-    private static final String REGISTRY_EUREKA_APPS = "/registry/eureka/apps/";
+    // TODO: do not hard-code 'registry' application id.
+    private static final String REGISTRY_REST_CALL_PREFIX = "/registry/eureka/apps/";
+
     private static final Logger LOG = LoggerFactory.getLogger(WhiteListFilter.class);
 
     private final WhiteList whitelist;
@@ -47,28 +47,27 @@ public class WhiteListFilter extends ZuulFilter {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest req = ctx.getRequest();
         String uri = req.getRequestURI();
-        boolean doRegister = doAimRegistry(ctx, uri);
-        boolean isOnWhitelist = isOnWhitelist(uri);
-        if (doRegister && !isOnWhitelist) {
+        if (isCallToRegistryService(ctx, uri) && !canUseRegistryService(uri)) {
             ctx.setSendZuulResponse(false);
             LOG.info("Request '{}' from {}:{} is blocked", uri, req.getRemoteHost(), req.getRemotePort());
         }
         return null;
     }
 
-    private boolean doAimRegistry(final RequestContext ctx, final String uri) {
-        return uri.contains(REGISTRY_EUREKA_APPS)
+    private boolean isCallToRegistryService(final RequestContext ctx, final String uri) {
+        return uri.contains(REGISTRY_REST_CALL_PREFIX)
                 && (RequestMethod.POST.toString().equals(ctx.getRequest().getMethod())
                     || RequestMethod.PUT.toString().equals(ctx.getRequest().getMethod()));
     }
 
-    private boolean isOnWhitelist(final String uri) {
+    private boolean canUseRegistryService(final String uri) {
         boolean result = false;
-        if (uri.split(REGISTRY_EUREKA_APPS).length == 0) {
+        if (uri.split(REGISTRY_REST_CALL_PREFIX).length == 0) {
             result = true;
-        } else if (uri.split(REGISTRY_EUREKA_APPS).length > 1) {
-            String s = uri.split(REGISTRY_EUREKA_APPS)[1];
-            result = whitelist.contains(s.split("/")[0]);
+        } else if (uri.split(REGISTRY_REST_CALL_PREFIX).length > 1) {
+            String registryCallSuffix = uri.split(REGISTRY_REST_CALL_PREFIX)[1];
+            String appId = registryCallSuffix.split("/")[0];
+            result = whitelist.contains(appId);
         }
         return result;
     }
